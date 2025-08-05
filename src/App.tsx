86394574f4066db4e7b78e602cf832fc717c32fa
mfjaconis <matheusjaconis@gmail.com>
2025-08-04 21:07:48 -0300

    feat: rotas para os componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import { Route, Routes } from "react-router-dom";
 import Banner from "./components/Banner";
 import Header from "./components/Header";
 import { GlobalStyle } from "./styles";
+import ProductList from "./components/ProductsList";
 
 function App() {
   return (
@@ -9,7 +11,18 @@ function App() {
       <div className="container">
         <Header />
       </div>
-      <Banner />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Banner />
+              <ProductList title="Promoções" background="gray" />
+              <ProductList title="Em breve" background="black" />
+            </>
+          }
+        />
+      </Routes>
     </>
   );
 }
